043f52b5c81070fe252a8d00cae816a838ff534a
ApikGamBom <east123beast@gmail.com>
2024-04-20 00:55:49 +0200

    Added sliders
    Added the ability to type input next to the slider to get exact numers(Was a BIG pain in the asshole)

--- Edging Beans 2.0/Assets/Scripts/SliderControl.cs
+++ Edging Beans 2.0/Assets/Scripts/SliderControl.cs
@@ -2,15 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class SliderControl : MonoBehaviour
 {
-    public TextMeshProUGUI  MouseSens;
-    public TextMeshProUGUI  ResumeDelay;
-    // Start is called before the first frame update
-    public void updateMouseSense(float value)
+    [Header("Mouse Sensitivity")]
+    [SerializeField] private TMP_InputField MouseSensInputField;
+    [SerializeField] private Slider MouseSensSlider;
+    [SerializeField] private float MouseSensMaxValue = 5;
+
+    [Header("Resume Delay")]
+    [SerializeField] private TMP_InputField ResumeDelayInputField;
+    [SerializeField] private Slider ResumeDelaySlider;
+    [SerializeField] private float ResumeDelayMaxValue = 10;
+    void Start()
     {
-        float mouseSenseLocalValue = value;
-        MouseSens.text = mouseSenseLocalValue.ToString("0.0");
+        MouseSensInputField.onEndEdit.AddListener(delegate { ValidateAndSetMouseSens(MouseSensInputField.text); });
+        MouseSensSlider.onValueChanged.AddListener(delegate(float value) { UpdateMouseSense(value); });
+
+        ResumeDelayInputField.onEndEdit.AddListener(delegate { ValidateAndSetResumeDelay(ResumeDelayInputField.text); });
+        ResumeDelaySlider.onValueChanged.AddListener(delegate(float value) { UpdateResumeDelay(value); });
+        
+    }
+
+    public void UpdateMouseSense(float value)
+    {
+        float scaledValue = value * MouseSensMaxValue;
+        MouseSensInputField.text = scaledValue.ToString("0.0");
+    }
+    private void ValidateAndSetMouseSens(string input)
+    {
+
+        if (float.TryParse(input, out float value))
+        {
+            if (value >= 0 && value <= MouseSensMaxValue)
+            {
+            MouseSensSlider.value = value / MouseSensMaxValue;
+            }
+            else
+            {
+                Debug.Log("Input out of range");
+                MouseSensInputField.text = (MouseSensSlider.value * MouseSensMaxValue).ToString("0.0");
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid input");
+            MouseSensInputField.text = (MouseSensSlider.value * MouseSensMaxValue).ToString("0.0");
+        }
+    }
+
+    public void UpdateResumeDelay(float value)
+    {
+        float scaledValue = value * ResumeDelayMaxValue;
+        ResumeDelayInputField.text = scaledValue.ToString("0.0");
+    }
+    private void ValidateAndSetResumeDelay(string input)
+    {
+
+        if (float.TryParse(input, out float value))
+        {
+            if (value >= 0 && value <= ResumeDelayMaxValue)
+            {
+            ResumeDelaySlider.value = value / ResumeDelayMaxValue;
+            }
+            else
+            {
+                Debug.Log("Input out of range");
+                ResumeDelayInputField.text = (ResumeDelaySlider.value * ResumeDelayMaxValue).ToString("0.0");
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid input");
+            ResumeDelayInputField.text = (ResumeDelaySlider.value * ResumeDelayMaxValue).ToString("0.0");
+        }
     }
 }
+ 
