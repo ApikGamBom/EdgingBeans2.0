ef18737f9b136689c5f0c88ce08ec77ac66fdb31
ApikGamBom <east123beast@gmail.com>
2024-04-20 16:04:52 +0200

    updated sliders
    added volume for gameplay and music. Slight update on the script

--- Edging Beans 2.0/Assets/Scripts/SliderControl.cs
+++ Edging Beans 2.0/Assets/Scripts/SliderControl.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 
 public class SliderControl : MonoBehaviour
 {
+    [Header("All")]
+    [SerializeField] private float lowestValue = 0.001f;
+
+
+
     [Header("Mouse Sensitivity")]
     [SerializeField] private TMP_InputField MouseSensInputField;
     [SerializeField] private Slider MouseSensSlider;
@@ -15,14 +20,34 @@ public class SliderControl : MonoBehaviour
     [SerializeField] private TMP_InputField ResumeDelayInputField;
     [SerializeField] private Slider ResumeDelaySlider;
     [SerializeField] private float ResumeDelayMaxValue = 10;
+    
+    [Header("Music Volume")]
+    [SerializeField] private TMP_InputField MusicVolumeInputField;
+    [SerializeField] private Slider MusicVolumeSlider;
+    [SerializeField] private float MusicVolumeMaxValue = 100;
+
+    [Header("Gameplay Volume")]
+    [SerializeField] private TMP_InputField GameplayVolumeInputField;
+    [SerializeField] private Slider GameplayVolumeSlider;
+    [SerializeField] private float GameplayVolumeMaxValue = 100;
     void Start()
     {
+
         MouseSensInputField.onEndEdit.AddListener(delegate { ValidateAndSetMouseSens(MouseSensInputField.text); });
         MouseSensSlider.onValueChanged.AddListener(delegate(float value) { UpdateMouseSense(value); });
+        UpdateMouseSense(MouseSensSlider.value);
 
         ResumeDelayInputField.onEndEdit.AddListener(delegate { ValidateAndSetResumeDelay(ResumeDelayInputField.text); });
         ResumeDelaySlider.onValueChanged.AddListener(delegate(float value) { UpdateResumeDelay(value); });
+        UpdateResumeDelay(ResumeDelaySlider.value);
         
+        MusicVolumeInputField.onEndEdit.AddListener(delegate { ValidateAndSetMusicVolume(MusicVolumeInputField.text); });
+        MusicVolumeSlider.onValueChanged.AddListener(delegate(float value) { UpdateMusicVolume(value); });
+        UpdateMusicVolume(MusicVolumeSlider.value);
+
+        GameplayVolumeInputField.onEndEdit.AddListener(delegate { ValidateAndSetGameplayVolume(ResumeDelayInputField.text); });
+        GameplayVolumeSlider.onValueChanged.AddListener(delegate(float value) { UpdateGameplayVolume(value); });
+        UpdateGameplayVolume(GameplayVolumeSlider.value);
     }
 
     public void UpdateMouseSense(float value)
@@ -35,7 +60,7 @@ public class SliderControl : MonoBehaviour
 
         if (float.TryParse(input, out float value))
         {
-            if (value >= 0 && value <= MouseSensMaxValue)
+            if (value >= lowestValue && value <= MouseSensMaxValue)
             {
             MouseSensSlider.value = value / MouseSensMaxValue;
             }
@@ -52,6 +77,8 @@ public class SliderControl : MonoBehaviour
         }
     }
 
+
+
     public void UpdateResumeDelay(float value)
     {
         float scaledValue = value * ResumeDelayMaxValue;
@@ -62,7 +89,7 @@ public class SliderControl : MonoBehaviour
 
         if (float.TryParse(input, out float value))
         {
-            if (value >= 0 && value <= ResumeDelayMaxValue)
+            if (value >= lowestValue && value <= ResumeDelayMaxValue)
             {
             ResumeDelaySlider.value = value / ResumeDelayMaxValue;
             }
@@ -76,6 +103,62 @@ public class SliderControl : MonoBehaviour
         {
             Debug.Log("Invalid input");
             ResumeDelayInputField.text = (ResumeDelaySlider.value * ResumeDelayMaxValue).ToString("0.0");
+        }
+    }
+
+    public void UpdateMusicVolume(float value)
+    {
+        float scaledValue = value * MusicVolumeMaxValue;
+        MusicVolumeInputField.text = scaledValue.ToString("0.0");
+    }
+    private void ValidateAndSetMusicVolume(string input)
+    {
+
+        if (float.TryParse(input, out float value))
+        {
+            if (value >= lowestValue && value <= MusicVolumeMaxValue)
+            {
+            MusicVolumeSlider.value = value / MusicVolumeMaxValue;
+            }
+            else
+            {
+                Debug.Log("Input out of range");
+                MusicVolumeInputField.text = (MusicVolumeSlider.value * MusicVolumeMaxValue).ToString("0.0");
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid input");
+            MusicVolumeInputField.text = (MusicVolumeSlider.value * MusicVolumeMaxValue).ToString("0.0");
+        }
+    }
+
+
+
+    public void UpdateGameplayVolume(float value)
+    {
+        float scaledValue = value * GameplayVolumeMaxValue;
+        GameplayVolumeInputField.text = scaledValue.ToString("0.0");
+    }
+    private void ValidateAndSetGameplayVolume(string input)
+    {
+
+        if (float.TryParse(input, out float value))
+        {
+            if (value >= lowestValue && value <= GameplayVolumeMaxValue)
+            {
+            GameplayVolumeSlider.value = value / GameplayVolumeMaxValue;
+            }
+            else
+            {
+                Debug.Log("Input out of range");
+                GameplayVolumeInputField.text = (GameplayVolumeSlider.value * GameplayVolumeMaxValue).ToString("0.0");
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid input");
+            GameplayVolumeInputField.text = (GameplayVolumeSlider.value * GameplayVolumeMaxValue).ToString("0.0");
         }
     }
 }
