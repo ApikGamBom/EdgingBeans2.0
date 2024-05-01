ba67dc2350d3b2d86df5f8b1684de816eb21ff18
ApikGamBom <east123beast@gmail.com>
2024-05-01 22:09:04 +0200

    idk

--- Edging Beans 2.0/Assets/Scripts/MainMenu.cs
+++ Edging Beans 2.0/Assets/Scripts/MainMenu.cs
@@ -6,20 +6,44 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Slid Controller")]
+    public SliderControl sliderController;
+
+    [Header("GameObjects")]
+    public GameObject pauseMenu;
+    public GameObject Crosshair;
+    public GameObject optionsTab;
+
+    [Header("Floats")]
+    public float UiCountdown;
+
+    [Header("Bools")]
+    public static bool isPaused = false;
+    public bool CountDone;
+    public bool optionOpen = false;
+
+    [Header("Other")]
     public GameObject mainMenu;
     
+    public void Update()
+    {
+        UiCountdown = sliderController.ResumeDelayValue;
+    }
     public void playGame()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("GameScene");
     }
 
     public void openSettings()
     {
-
+        optionsTab.SetActive(!optionsTab.activeSelf);
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        optionOpen = !optionOpen;
     }
 
     public void quitGame()
     {
-
+        Application.Quit();
     }
 }
